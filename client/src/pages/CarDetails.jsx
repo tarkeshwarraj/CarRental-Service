ae1756411aa6f1d9982209433e3d75ea1551e4ba
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-08-15 13:43:19 +0530

    cardetails page

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 
 const CarDetails = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate()
+  const [car, setCar] = React.useState(null);
+
+  
+
   return (
-    <div>CarDetails</div>
+    <div>
+      
+    </div>
   )
 }
 
